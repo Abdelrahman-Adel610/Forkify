790287880ee0ac92592abfd0d4242e38920b3271
Abdelrahman-Adel610 <engba80818233@gmail.com>
2024-09-18 00:36:17 +0300

    🔑 Update API_KEY

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const API_LINK = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_LIMIT = 10;
 export const ITEMS_PER_PAGE = 10;
-export const API_KEY = `f31a2476-a3d9-496d-abba-7f620e4ba5e0`;
+export const API_KEY = `d9dfe233-cf74-4fca-a7ae-6df374de33d8`;
 export const SUCCESS_MSG_TIME = 1.5;
 export const SPINNER_TIME = 0.5;
